18d12f19652214c4a7c65826351bf18c9b3635db
Clickbait-4587 <70538586+Clickbait-4587@users.noreply.github.com>
2020-10-26 06:03:59 +0200

    Update signup.js

--- docs/signup.js
+++ docs/signup.js
@@ -5,8 +5,8 @@ $(document).ready(() => {
     const password = signupForm["password"].value;
     const email = signupForm["email"].value;
     auth.createUserWithEmailAndPassword(email, password).then((resp) => {
-      window.location.href = "https://clickbait-4587.github.io/ShoppingCart";
-      console.log(resp);
+     window.location.href =
+      "https://clickbait-4587.github.io/ShoppingCart/store"
       signupForm.reset();
     });
   });
