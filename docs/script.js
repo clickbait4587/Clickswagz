808b07ee714b9130b8642b33421c7c2dc5232cd7
Clickbait-4587 <70538586+Clickbait-4587@users.noreply.github.com>
2020-10-28 04:38:19 +0200

    Women catagory

--- docs/script.js
+++ docs/script.js
@@ -6,44 +6,7 @@ let cartDOM = document.querySelector(".cart-items");
 let cart = [];
 let buttonsDOM = [];
 let user = "";
-let items = [
-  (item1 = {
-    category: "men clothing",
-    description:
-      "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-    id: 1,
-    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-    price: 109.95,
-    title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-  }),
-  (item2 = {
-    category: "men clothing",
-    description:
-      "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
-    id: 2,
-    image:
-      "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
-    price: 22.3,
-    title: "Mens Casual Premium Slim Fit T-Shirts ",
-  }),
-  (item3 = {
-    category: "men clothing",
-    description:
-      "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
-    id: 3,
-    image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
-    price: 55.99,
-  }),
-  (item4 = {
-    category: "men clothing",
-    description:
-      "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
-    id: 4,
-    image: "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
-    price: 15.99,
-    title: "Mens Casual Slim Fit",
-  }),
-];
+
 
 auth.onAuthStateChanged((usr) => {
   if (usr) {
@@ -63,8 +26,7 @@ class Product {
   async getProducts() {
     let res = await fetch("https://fakestoreapi.com/products");
     let data = await res.json();
-    data = data.slice(0, 0);
-    return data.filter(item=> item.catagory == "men clothing");
+    return data.filter(item=> item.category == "women clothing");
   }
 }
 class UI {
